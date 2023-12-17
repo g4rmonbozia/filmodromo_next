1b53497d9c241094f0b3333dd44c66086e2a081e
Gabriel <gabriel.antunes@al.infnet.edu.br>
2023-12-17 20:03:29 -0300

    Ajustes Responsividade 2

--- app/login/page.js
+++ app/login/page.js
@@ -2,18 +2,23 @@ import Link from "next/link";
 
 export default function Login() {
 	return (
-        <div className="mt-10 flex flex-row items-center">
+        <div className="my-10 flex flex-col items-center
+						lg:flex-row lg:items-start">
 			<div className="flex flex-col items-center space-y-6">
+				<p className="text-2xl">Faça seu login</p>
 				<input placeholder="E-mail" className="pl-2 h-10 w-72 border border-gray-700 rounded"></input>
 				<input placeholder="Senha" type="password" className="pl-2 h-10 w-72 border border-gray-700 rounded"></input>
 				<Link href="/" className="flex items-center justify-center h-10 w-72 border border-black bg-gray-400 text-white rounded">Entrar</Link>
 				<Link href="/" className="flex items-center justify-center h-10 w-72 border border-gray-400 bg-white text-black rounded">Entrar com Google</Link>
 			</div>
-			<div className="mr-10 ml-10 w-0 h-40 border-r-2 border-gray-400"></div>
+			<div className="my-6 w-40 h-0 border-t-2 border-gray-400
+							lg:mx-10 lg:w-0 lg:h-72 lg:border-r-2"></div>
 			<div className="flex flex-col items-center space-y-6">
+				<p className="text-2xl">... ou crie uma conta!</p>
 				<input placeholder="Nome" className="pl-2 h-10 w-72 border border-gray-700 rounded"></input>
 				<input placeholder="E-mail" className="pl-2 h-10 w-72 border border-gray-700 rounded"></input>
 				<input placeholder="Senha" type="password" className="pl-2 h-10 w-72 border border-gray-700 rounded"></input>
+				<input placeholder="Repetir Senha" type="password_2" className="pl-2 h-10 w-72 border border-gray-700 rounded"></input>
 				<Link href="/" className="flex items-center justify-center h-10 w-72 border border-black bg-gray-400 text-white rounded">Registrar</Link>
 			</div>
 		</div>
--- app/page.tsx
+++ app/page.tsx
@@ -7,16 +7,14 @@ export default function Home() {
                     lg:flex-row lg:h-6 lg:hover:h-20 lg:duration-300">
       <Link href="/filmes"
             className="flex items-center justify-end overflow-hidden text-white bg-yellow duration-300 flex-auto h-14
-                        lg:text-yellow lg:hover:text-white lg:hover:bg-white
-                        lg:hover:w-5/6 lg:w-1/6">
+                        lg:text-yellow lg:hover:text-white lg:hover:bg-white lg:hover:w-5/6 lg:w-1/6 lg:h-auto">
         <p className="font-extrabold text-3xl text-white
                       lg:text-5xl lg:text-yellow lg:-mb-2
                       xl:text-6xl">FILMES</p>
       </Link>
       <Link href="/"
             className="group flex items-center justify-end overflow-hidden text-white bg-cyan duration-300 flex-auto h-14
-                        lg:text-cyan lg:hover:bg-white 
-                        lg:hover:w-5/6 lg:w-1/6">
+                        lg:text-cyan lg:hover:bg-white lg:hover:w-5/6 lg:w-1/6 lg:h-auto">
         <div className="mr-2 h-6 w-20 rounded bg-white text-cyan font-extrabold text-sm flex items-center justify-center
                         lg:bg-cyan lg:group-hover:text-white
                         xl:-mb-2"><p>EM BREVE</p></div>
@@ -26,8 +24,7 @@ export default function Home() {
       </Link>
       <Link href="/"
             className="group flex items-center justify-end overflow-hidden text-white bg-green duration-300 flex-auto h-14
-                        lg:text-green lg:hover:bg-white
-                        lg:hover:w-5/6 lg:w-1/6">
+                        lg:text-green lg:hover:bg-white lg:hover:w-5/6 lg:w-1/6 lg:h-auto">
         <div className="mr-2 h-6 w-20 rounded bg-white text-green font-extrabold text-sm flex items-center justify-center
                         lg:bg-green lg:group-hover:text-white
                         xl:-mb-2"><p>EM BREVE</p></div>
@@ -37,8 +34,7 @@ export default function Home() {
       </Link>
       <Link href="/"
             className="group flex items-center justify-end overflow-hidden text-white bg-magenta duration-300 flex-auto h-14
-                        lg:text-magenta lg:hover:text-white lg:hover:bg-white
-                        lg:hover:w-5/6 lg:w-1/6">
+                        lg:text-magenta lg:hover:text-white lg:hover:bg-white lg:hover:w-5/6 lg:w-1/6 lg:h-auto">
         <div className="mr-2 h-6 w-20 rounded bg-white text-magenta font-extrabold text-sm flex items-center justify-center
                         lg:bg-magenta lg:group-hover:text-white
                         xl:-mb-2"><p>EM BREVE</p></div>
@@ -48,8 +44,7 @@ export default function Home() {
       </Link>
       <Link href="/"
             className="group flex items-center justify-end overflow-hidden text-white bg-red duration-300 flex-auto h-14
-                        lg:text-red lg:hover:text-white lg:hover:bg-white
-                        lg:hover:w-5/6 lg:w-1/6">
+                        lg:text-red lg:hover:text-white lg:hover:bg-white lg:hover:w-5/6 lg:w-1/6 lg:h-auto">
         <div className="mr-2 h-6 w-20 rounded bg-white text-red font-extrabold text-sm flex items-center justify-center
                         lg:bg-red lg:group-hover:text-white
                         xl:-mb-2"><p>EM BREVE</p></div>
@@ -59,8 +54,7 @@ export default function Home() {
       </Link>
       <Link href="/sobre"
             className="flex items-center justify-end overflow-hidden text-white bg-blue duration-300 flex-auto h-14
-                        lg:text-blue lg:hover:text-white lg:hover:bg-white
-                        lg:hover:w-5/6 lg:w-1/6">
+                        lg:text-blue lg:hover:text-white lg:hover:bg-white lg:hover:w-5/6 lg:w-1/6 lg:h-auto">
         <p className="font-extrabold text-3xl text-white
                       lg:text-5xl lg:text-blue lg:-mb-2
                       xl:text-6xl">SOBRE</p>
