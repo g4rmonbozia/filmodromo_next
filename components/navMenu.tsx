20d4cd413dbd498d5ed97acfe906102da8d08ae8
Gabriel Antunes <148727704+g4rmonbozia@users.noreply.github.com>
2023-12-19 18:03:43 +0000

    Menu Corrigido

--- components/navMenu.tsx
+++ components/navMenu.tsx
@@ -16,16 +16,15 @@ export default function NavMenu(){
 
 
         return(
-                <div>
-                        <button onClick={handleClick} className="absolute rounded-full mt-13 ml-10 bg-black flex items-center justify-center w-14 h-14 lg:hidden">
+                <div className="absolute w-full">
+                        <button onClick={handleClick} className="absolute z-20 rounded-full mt-[52px] ml-10 bg-black flex items-center justify-center w-14 h-14 lg:hidden">
                                 <svg className="fill-white w-8 h-8" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
                         </button>
-                        <div className={`absolute z-0 duration-300 select-none flex flex-col w-full ${toggleState}
-                                        ${isTouch == 1 ? "lg:h-20" : "lg:h-6 lg:hover:h-20"}
-                                        lg:mt-40 lg:flex-row`}>
+                        <div className={`z-0 duration-300 select-none flex flex-col w-full ${toggleState}
+                                        lg:h-6 lg:hover:h-20 lg:mt-40 lg:flex-row`}>
                                 <Link href="/filmes"
                                         className={`flex items-center justify-end overflow-hidden text-white bg-yellow duration-300 flex-auto h-12
-                                                        ${isTouch == 1 ? "lg:w-1/6" : "lg:text-yellow lg:hover:text-white lg:hover:bg-white lg:hover:w-5/6 lg:w-1/6 lg:h-auto"}`}>
+                                                        lg:text-yellow lg:hover:bg-white lg:hover:w-5/6 lg:w-1/6 lg:h-auto`}>
                                         <p className="font-extrabold text-3xl text-white
                                                 lg:text-5xl lg:text-yellow lg:-mb-2
                                                 xl:text-6xl">FILMES</p>
